91a4cb30efdc06e3c35425140556b0a3102f6aa2
Joey Hines <joey@ahines.net>
2019-09-02 15:10:18 -0500

    Organized HasCore into a class

--- home_automation_system/message_listener.py
+++ home_automation_system/message_listener.py
@@ -2,45 +2,78 @@ import asyncio
 import importlib
 import json
 
+class Packet:
+    def __init__(self, type, sender, size, message):
+        self.type = type
+        self.sender = sender
+        self.size = size
+        self.message = message
 
-async def handle_messages(reader, writer):
-    type = (await reader.readexactly(4)).decode()
-    sender = (await reader.readexactly(4)).decode()
-    size = int((await reader.readexactly(4)).decode())
 
-    message = (await reader.read(size)).decode()
+class HASCore:
+    def __init__(self, port=8888, ip='127.0.0.1'):
+        self.loop = asyncio.get_event_loop()
+        self.coro = asyncio.start_server(self.handle_messages, ip, port, loop=self.loop)
+        self.server = self.loop.run_until_complete(self.coro)
 
-    msg_json = json.loads(message)
+    async def handle_messages(self, reader, writer):
+        message = await reader.read()
 
-    addr = writer.get_extra_info('peername')
-    print("Received from %r:\n %r" % (addr, msg_json))
+        packet = self.parse_packet(message)
 
-    test = importlib.import_module('home_automation_system.rules.test_rules')
+        msg_json = json.loads(packet.message)
 
-    ruleset = test.get_ruleset()
+        addr = writer.get_extra_info('peername')
+        print("Received from %r:\n %r" % (addr, msg_json))
 
-    await ruleset.run_rules(msg_json["data"], **msg_json["rules"])
+        test = importlib.import_module('home_automation_system.rules.test_rules')
 
-    print("Close the client socket")
-    writer.close()
+        ruleset = test.get_ruleset()
+
+        await ruleset.run_rules(msg_json["data"], **msg_json["rules"])
+
+        print("Close the client socket")
+        writer.close()
+
+    async def send_packet(self, message, ip, port):
+        response = self.loop.run_until_complete(self._send(message, ip, port))
+
+        return response, len(response)
+
+    async def _send(self, message, ip, port):
+        reader, writer = await asyncio.open_connection(ip, port)
+
+        writer.write(message)
+        writer.write_eof()
+
+        response = await reader.read()
+        return response
+
+    def parse_packet(self, message):
+        type = message[0:3]
+        sender = message[4:7]
+        size = message[8:11]
+
+        message = message[12: size + 12]
+
+        packet = Packet(type, sender, size, message)
+
+        return packet
 
 
 def startup():
-    loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handle_messages, '127.0.0.1', 8888, loop=loop)
-    server = loop.run_until_complete(coro)
-
+    has = HASCore()
     # Serve requests until Ctrl+C is pressed
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    print('Serving on {}'.format(has.server.sockets[0].getsockname()))
     try:
-        loop.run_forever()
+        has.loop.run_forever()
     except KeyboardInterrupt:
         pass
 
     # Close the server
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
+    has.server.close()
+    has.loop.run_until_complete(has.server.wait_closed())
+    has.loop.close()
 
 
 if __name__ == "__main__":
